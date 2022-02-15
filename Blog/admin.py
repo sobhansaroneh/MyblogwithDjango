5c86b6e0ad6fbdcdcb5febe2840f7ed8b05e4810
Sobhan Saroneh <sobhansan20@gmail.com>
2022-02-16 00:03:29 +0330

    Register the models in admin and customize admin dashbord

--- Blog/admin.py
+++ Blog/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 from Blog.models import Article, Author
 
+class DetailAuthor(admin.ModelAdmin):
+    list_display = ['Last_name' , 'join_date']
+    list_filter = ('join_date',)
 
 
-admin.site.register(Author)
+admin.site.register(Author , DetailAuthor)
 admin.site.register(Article)
--- Blog/models.py
+++ Blog/models.py
@@ -1,15 +1,24 @@
 from time import timezone
+from django.contrib.auth.models import User
 from django.db import models
 from django.forms import CharField
 from ckeditor.fields import RichTextField
 class Author (models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
     First_name = models.CharField(max_length=20)
     Last_name = models.CharField(max_length=40)
     description =  RichTextField(max_length=250)
     join_date = models.DateField(auto_now=True)
+    
+
+    def __str__(self):
+        return f"{self.First_name} {self.Last_name}"
 
 
 class Article(models.Model):
     title = models.CharField(max_length=30)
     content  = RichTextField(max_length=1000)
     created = models.DateField(auto_now=True)
+    writer = models.ForeignKey('Author' , on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.title}"
