3c87ab40fc7c4574322f3fb95e5fb8be8593915f
Sobhan Saroneh <sobhansan20@gmail.com>
2022-02-16 18:22:48 +0330

    work with the models\

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,12 +1,22 @@
+from atexit import register
+from faulthandler import is_enabled
 from pyexpat import model
+from xml.dom.expatbuilder import Rejecter
 from django.contrib import admin
 
 from Blog.models import Article, Author
 
-class DetailAuthor(admin.ModelAdmin):
-    list_display = ['Last_name' , 'join_date']
-    list_filter = ('join_date',)
 
+class DetailAuthor(admin.ModelAdmin):
+    list_display = ['Last_name' , 'join_date','is_active']
+    list_filter = ('join_date','is_active')
+    search_fields = ('First_name' , 'Last_name')
+    list_editable = ['is_active']
+    actions = ['act']
+
+    def act(self , request , queryset):
+        pass
 
 admin.site.register(Author , DetailAuthor)
+
 admin.site.register(Article)
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from time import timezone
 from django.contrib.auth.models import User
 from django.db import models
@@ -5,15 +6,17 @@ from django.forms import CharField
 from ckeditor.fields import RichTextField
 class Author (models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
-    First_name = models.CharField(max_length=20)
-    Last_name = models.CharField(max_length=40)
+    First_name = models.CharField(max_length=20 , verbose_name='نام')
+    Last_name = models.CharField(max_length=40 , verbose_name='نام خانوادگی')
     description =  RichTextField(max_length=250)
     join_date = models.DateField(auto_now=True)
-    
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.First_name} {self.Last_name}"
-
+    class Meta:
+        verbose_name = 'نویسنده'
+        verbose_name_plural = "نویسنده‌ها"
 
 class Article(models.Model):
     title = models.CharField(max_length=30)
